fb24f2ceb817077a96b6a952baabe55af3d34295
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:34:21 +0530

    Create 1993-SumOfAllSubsetXorTotals

---
+++ 1993-SumOfAllSubsetXorTotals/1993-SumOfAllSubsetXorTotals.py
@@ -0,0 +1,12 @@
+# Last updated: 6/2/2025, 6:34:13 PM
+class Solution:
+  def subsetXORSum(self, nums: list[int]) -> int:
+    def dfs(i: int, xors: int) -> int:
+      if i == len(nums):
+        return xors
+
+      x = dfs(i + 1, xors)
+      y = dfs(i + 1, nums[i] ^ xors)
+      return x + y
+
+    return dfs(0, 0)
