01469f39085bb627236c865a5426f7c49fab81c4
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:35:15 +0530

    Create 1500-CountLargestGroup

---
+++ 1500-CountLargestGroup/1500-CountLargestGroup.py
@@ -0,0 +1,10 @@
+# Last updated: 6/2/2025, 6:35:08 PM
+class Solution:
+  def countLargestGroup(self, n: int) -> int:
+    count = [0] * (9 * 4 + 1)
+    for i in range(1, n + 1):
+      count[self._getDigitSum(i)] += 1
+    return count.count(max(count))
+
+  def _getDigitSum(self, num: int) -> int:
+    return sum(int(digit) for digit in str(num))
