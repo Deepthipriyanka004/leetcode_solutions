291b6edb505abe4ec231ec245b7b52a3c4584210
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:34:03 +0530

    Create 2076-SumOfDigitsOfStringAfterConvert

---
+++ 2076-SumOfDigitsOfStringAfterConvert/2076-SumOfDigitsOfStringAfterConvert.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/2/2025, 6:33:56 PM
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string t;
+        for (char c : s) t += to_string(c - 'a' + 1);
+        s = t;
+        while (k--) {
+            int t = 0;
+            for (char c : s) t += c - '0';
+            s = to_string(t);
+        }
+        return stoi(s);
+    }
+};
