cb1f76bb8213ec34d5ea052335cea76f8f99904d
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:35:19 +0530

    Create 1484-LinkedListInBinaryTree

---
+++ 1484-LinkedListInBinaryTree/1484-LinkedListInBinaryTree.cpp
@@ -0,0 +1,42 @@
+// Last updated: 6/2/2025, 6:35:12 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  bool isSubPath(ListNode *head, TreeNode *root) {
+    if (!head)
+      return true;
+    if (!root)
+      return false;
+
+    return f(head, root) || isSubPath(head, root->left) ||
+           isSubPath(head, root->right);
+  }
+
+  bool f(ListNode *head, TreeNode *root) {
+    if (!head)
+      return true;
+    if (!root)
+      return false;
+
+    if (head->val != root->val)
+      return false;
+
+    return f(head->next, root->left) || f(head->next, root->right);
+  }
+};
