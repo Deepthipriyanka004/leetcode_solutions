db5ab04206ef7d6193bdd215b3aa557b1c02273c
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:35:45 +0530

    Create 1335-MaximumCandiesAllocatedToKChildren

---
+++ 1335-MaximumCandiesAllocatedToKChildren/1335-MaximumCandiesAllocatedToKChildren.py
@@ -0,0 +1,17 @@
+# Last updated: 6/2/2025, 6:35:38 PM
+class Solution:
+  def maximumCandies(self, candies: list[int], k: int) -> int:
+    l = 1
+    r = sum(candies) // k
+
+    def numChildren(m: int) -> bool:
+      return sum(c // m for c in candies)
+
+    while l < r:
+      m = (l + r) // 2
+      if numChildren(m) < k:
+        r = m
+      else:
+        l = m + 1
+
+    return l if numChildren(l) >= k else l - 1
