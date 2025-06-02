05916028c18651b13761dd070ec69e70037f8d82
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:32:54 +0530

    Create 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks

---
+++ 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks/2463-MinimumRecolorsToGetKConsecutiveBlackBlocks.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/2/2025, 6:32:46 PM
+class Solution {
+ public:
+  int minimumRecolors(string blocks, int k) {
+    int countB = 0;
+    int maxCountB = 0;
+
+    for (int i = 0; i < blocks.length(); ++i) {
+      if (blocks[i] == 'B')
+        ++countB;
+      if (i >= k && blocks[i - k] == 'B')
+        --countB;
+      maxCountB = max(maxCountB, countB);
+    }
+
+    return k - maxCountB;
+  }
+};
