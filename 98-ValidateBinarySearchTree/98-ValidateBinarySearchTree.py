28231a780dd5bfebd4dc6907a9ff9f5afa42e5a0
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:40:26 +0530

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -0,0 +1,16 @@
+# Last updated: 6/2/2025, 6:40:19 PM
+class Solution:
+  def isValidBST(self, root: TreeNode | None) -> bool:
+    def isValidBST(root: TreeNode | None,
+                   minNode: TreeNode | None, maxNode: TreeNode | None) -> bool:
+      if not root:
+        return True
+      if minNode and root.val <= minNode.val:
+        return False
+      if maxNode and root.val >= maxNode.val:
+        return False
+
+      return (isValidBST(root.left, minNode, root) and
+              isValidBST(root.right, root, maxNode))
+
+    return isValidBST(root, None, None)
