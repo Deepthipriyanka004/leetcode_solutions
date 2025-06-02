1709e2fdc85c3ac893d291f80551fba4a54be5f1
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:42:28 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.py
@@ -0,0 +1,18 @@
+# Last updated: 6/2/2025, 6:42:20 PM
+class Solution:
+  def firstMissingPositive(self, nums: list[int]) -> int:
+    n = len(nums)
+
+    # Correct slot:
+    # nums[i] = i + 1
+    # nums[i] - 1 = i
+    # nums[nums[i] - 1] = nums[i]
+    for i in range(n):
+      while nums[i] > 0 and nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+        nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+    for i, num in enumerate(nums):
+      if num != i + 1:
+        return i + 1
+
+    return n + 1
