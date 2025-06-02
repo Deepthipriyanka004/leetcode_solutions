52f5870c5e05475bc145b90a9200a272be4badc1
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:42:57 +0530

    Create 23-MergeKSortedLists

---
+++ 23-MergeKSortedLists/23-MergeKSortedLists.py
@@ -0,0 +1,22 @@
+# Last updated: 6/2/2025, 6:42:49 PM
+from queue import PriorityQueue
+
+
+class Solution:
+  def mergeKLists(self, lists: list[ListNode]) -> ListNode:
+    dummy = ListNode(0)
+    curr = dummy
+    pq = PriorityQueue()
+
+    for i, lst in enumerate(lists):
+      if lst:
+        pq.put((lst.val, i, lst))
+
+    while not pq.empty():
+      _, i, minNode = pq.get()
+      if minNode.next:
+        pq.put((minNode.next.val, i, minNode.next))
+      curr.next = minNode
+      curr = curr.next
+
+    return dummy.next
