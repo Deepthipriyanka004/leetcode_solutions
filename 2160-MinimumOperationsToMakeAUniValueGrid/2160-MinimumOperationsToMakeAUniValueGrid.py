40e22a5f4cd402de837578abdfeb5445e1f5c754
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:33:53 +0530

    Create 2160-MinimumOperationsToMakeAUniValueGrid

---
+++ 2160-MinimumOperationsToMakeAUniValueGrid/2160-MinimumOperationsToMakeAUniValueGrid.py
@@ -0,0 +1,13 @@
+# Last updated: 6/2/2025, 6:33:46 PM
+class Solution:
+  def minOperations(self, grid: list[list[int]], x: int) -> int:
+    arr = sorted([a for row in grid for a in row])
+    if any((a - arr[0]) % x for a in arr):
+      return -1
+
+    ans = 0
+
+    for a in arr:
+      ans += abs(a - arr[len(arr) // 2]) // x
+
+    return ans
