c63eaee6cb2f994016e69aebc5ad98e89cf684ea
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:34:15 +0530

    Create 2050-CountGoodNumbers

---
+++ 2050-CountGoodNumbers/2050-CountGoodNumbers.py
@@ -0,0 +1,13 @@
+# Last updated: 6/2/2025, 6:34:08 PM
+class Solution:
+  def countGoodNumbers(self, n: int) -> int:
+    MOD = 1_000_000_007
+
+    def modPow(x: int, n: int) -> int:
+      if n == 0:
+        return 1
+      if n % 2 == 1:
+        return x * modPow(x, n - 1) % MOD
+      return modPow(x * x % MOD, n // 2)
+
+    return modPow(4 * 5, n // 2) * (1 if n % 2 == 0 else 5) % MOD
