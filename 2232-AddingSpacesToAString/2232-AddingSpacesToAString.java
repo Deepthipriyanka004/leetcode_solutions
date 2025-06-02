f98bf86d39d88a4139250426ba9e24655c14d4d3
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:33:28 +0530

    Create 2232-AddingSpacesToAString

---
+++ 2232-AddingSpacesToAString/2232-AddingSpacesToAString.java
@@ -0,0 +1,17 @@
+// Last updated: 6/2/2025, 6:33:21 PM
+class Solution {
+  public String addSpaces(String s, int[] spaces) {
+    StringBuilder sb = new StringBuilder();
+    int j = 0; // spaces' index
+
+    for (int i = 0; i < s.length(); ++i) {
+      if (j < spaces.length && i == spaces[j]) {
+        sb.append(' ');
+        ++j;
+      }
+      sb.append(s.charAt(i));
+    }
+
+    return sb.toString();
+  }
+}
