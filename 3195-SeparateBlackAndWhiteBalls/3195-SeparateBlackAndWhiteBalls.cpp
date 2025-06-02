ce00d038b2a1ccbeb4607a3485b76dfd4f0aebdd
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:31:11 +0530

    Create 3195-SeparateBlackAndWhiteBalls

---
+++ 3195-SeparateBlackAndWhiteBalls/3195-SeparateBlackAndWhiteBalls.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/2/2025, 6:31:04 PM
+class Solution {
+ public:
+  long long minimumSteps(string s) {
+    long ans = 0;
+    int ones = 0;
+
+    for (const char c : s)
+      if (c == '1')
+        ++ones;
+      else  // Move 1s to the front of the current '0'.
+        ans += ones;
+
+    return ans;
+  }
+};
