52894269658f9f34b2e4c2ec0e46e102a1c5972d
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:33:03 +0530

    Create 2394-CountSubarraysWithScoreLessThanK

---
+++ 2394-CountSubarraysWithScoreLessThanK/2394-CountSubarraysWithScoreLessThanK.py
@@ -0,0 +1,15 @@
+# Last updated: 6/2/2025, 6:32:55 PM
+class Solution:
+  def countSubarrays(self, nums: list[int], k: int) -> int:
+    ans = 0
+    summ = 0
+
+    l = 0
+    for r, num in enumerate(nums):
+      summ += num
+      while summ * (r - l + 1) >= k:
+        summ -= nums[l]
+        l += 1
+      ans += r - l + 1
+
+    return ans
