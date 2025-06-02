230fc76db484b7a831d7a021dd4742924ab21fec
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:39:51 +0530

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/2/2025, 6:39:44 PM
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> f(rowIndex + 1, 1);
+        for (int i = 2; i < rowIndex + 1; ++i) {
+            for (int j = i - 1; j; --j) {
+                f[j] += f[j - 1];
+            }
+        }
+        return f;
+    }
+};
