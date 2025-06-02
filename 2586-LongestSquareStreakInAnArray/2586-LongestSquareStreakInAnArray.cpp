d4ea3a4216bfb5fa2e88d0319c82694efdea2537
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:32:25 +0530

    Create 2586-LongestSquareStreakInAnArray

---
+++ 2586-LongestSquareStreakInAnArray/2586-LongestSquareStreakInAnArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 6/2/2025, 6:32:18 PM
+class Solution {
+ public:
+  int longestSquareStreak(vector<int>& nums) {
+    nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    ranges::sort(nums, greater<>());
+
+    const int maxNum = ranges::max(nums);
+    // dp[i] := the longest square streak starts with i
+    vector<int> dp(maxNum + 1);
+
+    for (const int num : nums) {
+      dp[num] = 1;
+      const long squaredNum = static_cast<long>(num) * num;
+      if (squaredNum <= maxNum)
+        dp[num] += dp[squaredNum];
+    }
+
+    const int ans = ranges::max(dp);
+    return ans < 2 ? -1 : ans;
+  }
+};
