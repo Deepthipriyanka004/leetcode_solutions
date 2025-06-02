5049906d7fb60af77dfc6d3db96820479ef79c86
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:40:01 +0530

    Create 113-PathSumIi

---
+++ 113-PathSumIi/113-PathSumIi.cpp
@@ -0,0 +1,30 @@
+// Last updated: 6/2/2025, 6:39:53 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> t;
+        function<void(TreeNode*, int)> dfs = [&](TreeNode* root, int s) {
+            if (!root) return;
+            s -= root->val;
+            t.emplace_back(root->val);
+            if (!root->left && !root->right && s == 0) ans.emplace_back(t);
+            dfs(root->left, s);
+            dfs(root->right, s);
+            t.pop_back();
+        };
+        dfs(root, targetSum);
+        return ans;
+    }
+};
