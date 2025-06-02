cff7511d2dfc6b5ec90c7ab597c778818adb9d7f
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:31:04 +0530

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.cpp
@@ -0,0 +1,25 @@
+// Last updated: 6/2/2025, 6:30:56 PM
+class Solution {
+ public:
+  vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+    const int n = grid.size();
+    const int nSquared = n * n;
+    vector<int> count(nSquared + 1);
+
+    for (const vector<int>& row : grid)
+      for (const int num : row)
+        ++count[num];
+
+    int repeated = -1;
+    int missing = -1;
+
+    for (int i = 1; i <= nSquared; ++i) {
+      if (count[i] == 2)
+        repeated = i;
+      if (count[i] == 0)
+        missing = i;
+    }
+
+    return {repeated, missing};
+  }
+};
