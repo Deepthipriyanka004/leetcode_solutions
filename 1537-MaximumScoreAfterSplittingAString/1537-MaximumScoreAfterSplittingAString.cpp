9ff5065d651a7f89e8f05e86fd5a5ea44ecce205
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:35:08 +0530

    Create 1537-MaximumScoreAfterSplittingAString

---
+++ 1537-MaximumScoreAfterSplittingAString/1537-MaximumScoreAfterSplittingAString.cpp
@@ -0,0 +1,19 @@
+// Last updated: 6/2/2025, 6:35:01 PM
+class Solution {
+ public:
+  int maxScore(string s) {
+    int ans = 0;
+    int zeros = 0;
+    int ones = ranges::count(s, '1');
+
+    for (int i = 0; i + 1 < s.size(); ++i) {
+      if (s[i] == '0')
+        ++zeros;
+      else
+        --ones;
+      ans = max(ans, zeros + ones);
+    }
+
+    return ans;
+  }
+};
