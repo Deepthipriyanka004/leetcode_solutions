0fc4dd5b771e6d499a8f7c6956a98a20620c7413
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:31:37 +0530

    Create 2915-CountOfInterestingSubarrays

---
+++ 2915-CountOfInterestingSubarrays/2915-CountOfInterestingSubarrays.py
@@ -0,0 +1,21 @@
+# Last updated: 6/2/2025, 6:31:30 PM
+class Solution:
+  def countInterestingSubarrays(
+      self,
+      nums: list[int],
+      modulo: int,
+      k: int,
+  ) -> int:
+    ans = 0
+    prefix = 0  # (number of nums[i] % modulo == k so far) % modulo
+    prefixCount = collections.Counter({0: 1})
+
+    for num in nums:
+      if num % modulo == k:
+        prefix = (prefix + 1) % modulo
+      ans += prefixCount[(prefix - k + modulo) % modulo]
+      prefixCount[prefix] += 1
+
+    return ans
+
+
