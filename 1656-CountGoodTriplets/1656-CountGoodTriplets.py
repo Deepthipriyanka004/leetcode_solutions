01a0e7394fd52cabce8742937bccad1c03868585
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:34:59 +0530

    Create 1656-CountGoodTriplets

---
+++ 1656-CountGoodTriplets/1656-CountGoodTriplets.py
@@ -0,0 +1,7 @@
+# Last updated: 6/2/2025, 6:34:51 PM
+class Solution:
+  def countGoodTriplets(self, arr: list[int], a: int, b: int, c: int) -> int:
+    return sum(abs(arr[i] - arr[j]) <= a and
+               abs(arr[j] - arr[k]) <= b and
+               abs(arr[i] - arr[k]) <= c
+               for i, j, k in itertools.combinations(range(len(arr)), 3))
