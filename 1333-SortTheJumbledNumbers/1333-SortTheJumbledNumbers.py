2a12e8bb3acfc6a563ac3b157c96f7d65274b009
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:35:47 +0530

    Create 1333-SortTheJumbledNumbers

---
+++ 1333-SortTheJumbledNumbers/1333-SortTheJumbledNumbers.py
@@ -0,0 +1,12 @@
+# Last updated: 6/2/2025, 6:35:39 PM
+class Solution:
+  def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+    def getMapped(num: int) -> int:
+      mapped = []
+      for c in str(num):
+        mapped.append(str(mapping[ord(c) - ord('0')]))
+        # mapped.append(str(mapping[ord(c)- ord('0')]))
+      return int(''.join(mapped))
+    A = [(getMapped(num), i, num) for i, num in enumerate(nums)]
+    return [num for _, i, num in sorted(A)]
+        
