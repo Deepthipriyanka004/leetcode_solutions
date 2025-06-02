0dc03552b1ce429644ff36cc68e533289901ddca
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:38:29 +0530

    Create 230-KthSmallestElementInABst

---
+++ 230-KthSmallestElementInABst/230-KthSmallestElementInABst.py
@@ -0,0 +1,15 @@
+# Last updated: 6/2/2025, 6:38:21 PM
+class Solution:
+  def kthSmallest(self, root: TreeNode | None, k: int) -> int:
+    def countNodes(root: TreeNode | None) -> int:
+      if not root:
+        return 0
+      return 1 + countNodes(root.left) + countNodes(root.right)
+
+    leftCount = countNodes(root.left)
+
+    if leftCount == k - 1:
+      return root.val
+    if leftCount >= k:
+      return self.kthSmallest(root.left, k)
+    return self.kthSmallest(root.right, k - 1 - leftCount)  # leftCount < k
