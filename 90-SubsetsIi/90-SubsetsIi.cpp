58a854081ba29878bcc20db9058905783f8a7051
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:40:40 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.cpp
@@ -0,0 +1,25 @@
+// Last updated: 6/2/2025, 6:40:32 PM
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> t;
+        int n = nums.size();
+        function<void(int)> dfs = [&](int i) {
+            if (i >= n) {
+                ans.push_back(t);
+                return;
+            }
+            t.push_back(nums[i]);
+            dfs(i + 1);
+            t.pop_back();
+            while (i + 1 < n && nums[i + 1] == nums[i]) {
+                ++i;
+            }
+            dfs(i + 1);
+        };
+        dfs(0);
+        return ans;
+    }
+};
