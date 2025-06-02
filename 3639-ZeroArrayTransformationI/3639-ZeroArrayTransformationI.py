7327cf81255aea9dc6b18a5f73d62cc022c793e8
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:30:05 +0530

    Create 3639-ZeroArrayTransformationI

---
+++ 3639-ZeroArrayTransformationI/3639-ZeroArrayTransformationI.py
@@ -0,0 +1,16 @@
+# Last updated: 6/2/2025, 6:29:58 PM
+class Solution:
+  def isZeroArray(self, nums: list[int], queries: list[list[int]]) -> bool:
+    line = [0] * (len(nums) + 1)
+    decrement = 0
+
+    for l, r in queries:
+      line[l] += 1
+      line[r + 1] -= 1
+
+    for i, num in enumerate(nums):
+      decrement += line[i]
+      if decrement < num:
+        return False
+
+    return True
