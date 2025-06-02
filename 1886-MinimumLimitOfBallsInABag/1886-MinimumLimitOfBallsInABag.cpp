52af298e4256f98f2dfedc1080096c0f80f2120d
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:34:37 +0530

    Create 1886-MinimumLimitOfBallsInABag

---
+++ 1886-MinimumLimitOfBallsInABag/1886-MinimumLimitOfBallsInABag.cpp
@@ -0,0 +1,27 @@
+// Last updated: 6/2/2025, 6:34:30 PM
+class Solution {
+ public:
+  int minimumSize(vector<int>& nums, int maxOperations) {
+    int l = 1;
+    int r = ranges::max(nums);
+
+    while (l < r) {
+      const int m = (l + r) / 2;
+      if (numOperations(nums, m) <= maxOperations)
+        r = m;
+      else
+        l = m + 1;
+    }
+
+    return l;
+  }
+
+ private:
+  // Returns the number of operations required to make m penalty.
+  int numOperations(const vector<int>& nums, int m) {
+    int operations = 0;
+    for (const int num : nums)
+      operations += (num - 1) / m;
+    return operations;
+  }
+};
