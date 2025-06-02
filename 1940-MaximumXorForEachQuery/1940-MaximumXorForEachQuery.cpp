05d5455c0b3c589148a42f0968fee821049b7991
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:34:26 +0530

    Create 1940-MaximumXorForEachQuery

---
+++ 1940-MaximumXorForEachQuery/1940-MaximumXorForEachQuery.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/2/2025, 6:34:19 PM
+class Solution {
+ public:
+  vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+    const int mx = (1 << maximumBit) - 1;
+    vector<int> ans;
+    int xors = 0;
+
+    for (const int num : nums) {
+      xors ^= num;
+      ans.push_back(xors ^ mx);
+    }
+
+    ranges::reverse(ans);
+    return ans;
+  }
+};
