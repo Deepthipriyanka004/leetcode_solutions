1cb26ee5ff5cef05a87b1129e7224496f4907cd6
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:37:26 +0530

    Create 515-FindLargestValueInEachTreeRow

---
+++ 515-FindLargestValueInEachTreeRow/515-FindLargestValueInEachTreeRow.cpp
@@ -0,0 +1,27 @@
+// Last updated: 6/2/2025, 6:37:18 PM
+class Solution {
+ public:
+  vector<int> largestValues(TreeNode* root) {
+    if (root == nullptr)
+      return {};
+
+    vector<int> ans;
+    queue<TreeNode*> q{{root}};
+
+    while (!q.empty()) {
+      int mx = INT_MIN;
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode* node = q.front();
+        q.pop();
+        mx = max(mx, node->val);
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
+      ans.push_back(mx);
+    }
+
+    return ans;
+  }
+};
