83632c0641a541ca189deb3d1c40a2a9983f77c9
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:38:52 +0530

    Create 179-LargestNumber

---
+++ 179-LargestNumber/179-LargestNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/2/2025, 6:38:44 PM
+class Solution {
+ public:
+  string largestNumber(vector<int>& nums) {
+    string ans;
+
+    ranges::sort(nums, [](int a, int b) {
+      return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+    });
+
+    for (const int num : nums)
+      ans += to_string(num);
+
+    return ans[0] == '0' ? "0" : ans;
+  }
+};
