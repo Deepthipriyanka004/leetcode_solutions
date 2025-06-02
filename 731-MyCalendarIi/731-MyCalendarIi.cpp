10e84d702703593307e6b65a25845af7427b5a94
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-02 18:37:02 +0530

    Create 731-MyCalendarIi

---
+++ 731-MyCalendarIi/731-MyCalendarIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/2/2025, 6:36:55 PM
+class MyCalendarTwo {
+ public:
+  bool book(int start, int end) {
+    for (const auto& [s, e] : overlaps)
+      if (max(start, s) < min(end, e))
+        return false;
+
+    for (const auto& [s, e] : ranges) {
+      const int ss = max(start, s);
+      const int ee = min(end, e);
+      if (ss < ee)
+        overlaps.emplace_back(ss, ee);
+    }
+
+    ranges.emplace_back(start, end);
+    return true;
+  }
+
+ private:
+  vector<pair<int, int>> ranges;
+  vector<pair<int, int>> overlaps;
+};
