1d5e8f43583c6a79d34d2ebf09501458c641e40c
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-06-22 08:40:32 +0530

    Create 2138-DivideaStringIntoGroupsofSizek

---
+++ 2138-DivideaStringIntoGroupsofSizek/2138-DivideaStringIntoGroupsofSizek.cpp
@@ -0,0 +1,14 @@
+// Last updated: 6/22/2025, 8:40:32 AM
+class Solution {
+ public:
+  vector<string> divideString(string s, int k, char fill) {
+    vector<string> ans;
+
+    for (int i = 0; i < s.length(); i += k)
+      ans.push_back(i + k > s.length()
+                        ? s.substr(i) + string(i + k - s.length(), fill)
+                        : s.substr(i, k));
+
+    return ans;
+  }
+};
