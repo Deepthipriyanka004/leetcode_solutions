835905a0acc66c21aefa694dad460fb23dea3678
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-07-11 15:05:16 +0530

    Create 2402-MeetingRoomsIII

---
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -0,0 +1,26 @@
+# Last updated: 7/11/2025, 3:05:17 PM
+class Solution:
+  def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
+    count = [0] * n
+
+    meetings.sort()
+
+    occupied = []  # (endTime, roomId)
+    availableRoomIds = [i for i in range(n)]
+    heapq.heapify(availableRoomIds)
+
+    for start, end in meetings:
+      # Push meetings ending before this `meeting` in occupied to the
+      # `availableRoomsIds`.
+      while occupied and occupied[0][0] <= start:
+        heapq.heappush(availableRoomIds, heapq.heappop(occupied)[1])
+      if availableRoomIds:
+        roomId = heapq.heappop(availableRoomIds)
+        count[roomId] += 1
+        heapq.heappush(occupied, (end, roomId))
+      else:
+        newStart, roomId = heapq.heappop(occupied)
+        count[roomId] += 1
+        heapq.heappush(occupied, (newStart + (end - start), roomId))
+
+    return count.index(max(count))
