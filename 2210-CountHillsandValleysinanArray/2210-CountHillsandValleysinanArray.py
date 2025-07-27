2cf43636f373a2065a7bc23c065ddfa4d8c32bb0
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-07-27 06:30:57 +0530

    Create 2210-CountHillsandValleysinanArray

---
+++ 2210-CountHillsandValleysinanArray/2210-CountHillsandValleysinanArray.py
@@ -0,0 +1,15 @@
+# Last updated: 7/27/2025, 6:30:56 AM
+class Solution:
+  def countHillValley(self, nums: list[int]) -> int:
+    ans = 0
+    left = nums[0]
+
+    for i in range(1, len(nums) - 1):
+      if (left < nums[i] and nums[i] > nums[i + 1] or  # the hill
+              left > nums[i] and nums[i] < nums[i + 1]):  # the valley
+        ans += 1
+        left = nums[i]
+
+    return ans
+
+
