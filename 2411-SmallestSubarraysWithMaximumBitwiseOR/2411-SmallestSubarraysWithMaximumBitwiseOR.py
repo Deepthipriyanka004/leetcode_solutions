a93d9cb0988db5746a9eca2199961e2ff06ecd78
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-07-29 18:34:34 +0530

    Create 2411-SmallestSubarraysWithMaximumBitwiseOR

---
+++ 2411-SmallestSubarraysWithMaximumBitwiseOR/2411-SmallestSubarraysWithMaximumBitwiseOR.py
@@ -0,0 +1,15 @@
+# Last updated: 7/29/2025, 6:34:31 PM
+class Solution:
+  def smallestSubarrays(self, nums: list[int]) -> list[int]:
+    MAX_BIT = 30
+    ans = [1] * len(nums)
+    # closest[j] := the closest index i s.t. the j-th bit of nums[i] is 1
+    closest = [0] * MAX_BIT
+
+    for i in reversed(range(len(nums))):
+      for j in range(MAX_BIT):
+        if nums[i] >> j & 1:
+          closest[j] = i
+        ans[i] = max(ans[i], closest[j] - i + 1)
+
+    return ans
