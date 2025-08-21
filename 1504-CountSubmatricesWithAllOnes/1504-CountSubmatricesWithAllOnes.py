c0b6c5eeab4045f3555abc82433b473543bcb613
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-08-21 08:33:43 +0530

    Create 1504-CountSubmatricesWithAllOnes

---
+++ 1504-CountSubmatricesWithAllOnes/1504-CountSubmatricesWithAllOnes.py
@@ -0,0 +1,23 @@
+# Last updated: 8/21/2025, 8:33:26 AM
+class Solution:
+  def numSubmat(self, mat: list[list[int]]) -> int:
+    m = len(mat)
+    n = len(mat[0])
+    ans = 0
+
+    for baseRow in range(m):
+      row = [1] * n
+      for i in range(baseRow, m):
+        for j in range(n):
+          row[j] &= mat[i][j]
+        ans += self._count(row)
+
+    return ans
+
+  def _count(self, row: list[int]) -> int:
+    res = 0
+    length = 0
+    for num in row:
+      length = 0 if num == 0 else length + 1
+      res += length
+    return res
