d7b07207d0a7cef293f0a0baf570e31e57adbe13
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-08-24 10:45:06 +0530

    Create 3491-FindTheMaximumLengthOfValidSubsequenceIi

---
+++ 3491-FindTheMaximumLengthOfValidSubsequenceIi/3491-FindTheMaximumLengthOfValidSubsequenceIi.py
@@ -0,0 +1,14 @@
+# Last updated: 8/24/2025, 10:44:43 AM
+class Solution:
+  # Similar to 3201. Find the Maximum Length of Valid Subsequence I
+  def maximumLength(self, nums: list[int], k: int) -> int:
+    # dp[i][j] := the maximum length of a valid subsequence, where the last
+    # number mod k equal to i and the next desired number mod k equal to j
+    dp = [[0] * k for _ in range(k)]
+
+    # Extend the pattern xyxyxy...xy.
+    for x in nums:
+      for y in range(k):
+        dp[x % k][y] = dp[y][x % k] + 1
+
+    return max(map(max, dp))
