1e433687327e2aa290e35ed932d52c522c7cf78a
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-08-24 10:45:48 +0530

    Create 2704-MaximumDifferenceByRemappingADigit

---
+++ 2704-MaximumDifferenceByRemappingADigit/2704-MaximumDifferenceByRemappingADigit.py
@@ -0,0 +1,13 @@
+# Last updated: 8/24/2025, 10:45:26 AM
+class Solution:
+  def minMaxDifference(self, num: int) -> int:
+    s = str(num)
+    to9 = s[self._firstNotNineIndex(s)]
+    to0 = s[0]
+    return int(s.replace(to9, '9')) - int(s.replace(to0, '0'))
+
+  def _firstNotNineIndex(self, s: str) -> int:
+    for i, c in enumerate(s):
+      if c != '9':
+        return i
+    return 0
