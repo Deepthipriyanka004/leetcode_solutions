0962d66bdfbb46b74974d66f2f7a67a06992c3e2
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-09-13 21:59:49 +0530

    Create 3541-FindMostFrequentVowelandConsonant

---
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.py
@@ -0,0 +1,8 @@
+# Last updated: 9/13/2025, 9:59:33 PM
+class Solution:
+  def maxFreqSum(self, s: str) -> int:
+    VOWELS = 'aeiou'
+    count = collections.Counter(s)
+    maxVowel = max((count[c] for c in VOWELS if c in count), default=0)
+    maxConsonant = max((count[c] for c in count if c not in VOWELS), default=0)
+    return maxVowel + maxConsonant
