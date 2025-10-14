d563a4ee74387a053cd9615e36eec43c36325c05
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-10-14 20:59:24 +0530

    Create 3349-AdjacentIncreasingSubarraysDetectionI

---
+++ 3349-AdjacentIncreasingSubarraysDetectionI/3349-AdjacentIncreasingSubarraysDetectionI.py
@@ -0,0 +1,16 @@
+# Last updated: 10/14/2025, 8:58:27 PM
+class Solution:
+  def hasIncreasingSubarrays(self, nums: list[int], k: int) -> bool:
+    increasing = 1
+    prevIncreasing = 0
+
+    for a, b in itertools.pairwise(nums):
+      if b > a:
+        increasing += 1
+      else:
+        prevIncreasing = increasing
+        increasing = 1
+      if increasing // 2 >= k or min(prevIncreasing, increasing) >= k:
+        return True
+
+    return False
