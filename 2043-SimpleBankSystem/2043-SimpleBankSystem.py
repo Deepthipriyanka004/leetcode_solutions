ff9dfc306195908559d89e21773ea4fdf64fe40f
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-10-26 11:53:11 +0530

    Create 2043-SimpleBankSystem

---
+++ 2043-SimpleBankSystem/2043-SimpleBankSystem.py
@@ -0,0 +1,26 @@
+# Last updated: 10/26/2025, 11:52:00 AM
+class Bank:
+  def __init__(self, balance: list[int]):
+    self.balance = balance
+
+  def transfer(self, account1: int, account2: int, money: int) -> bool:
+    if not self._isValid(account2):
+      return False
+    return self.withdraw(account1, money) and self.deposit(account2, money)
+
+  def deposit(self, account: int, money: int) -> bool:
+    if not self._isValid(account):
+      return False
+    self.balance[account - 1] += money
+    return True
+
+  def withdraw(self, account: int, money: int) -> bool:
+    if not self._isValid(account):
+      return False
+    if self.balance[account - 1] < money:
+      return False
+    self.balance[account - 1] -= money
+    return True
+   
+  def _isValid(self, account: int) -> bool:
+    return 1 <= account <= len(self.balance) 
