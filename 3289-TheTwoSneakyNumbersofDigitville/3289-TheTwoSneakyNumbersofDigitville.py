b72625d60851e3a5a8d0e0f3be4909f9642dc71d
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-10-31 21:14:03 +0530

    Create 3289-TheTwoSneakyNumbersofDigitville

---
+++ 3289-TheTwoSneakyNumbersofDigitville/3289-TheTwoSneakyNumbersofDigitville.py
@@ -0,0 +1,5 @@
+# Last updated: 10/31/2025, 9:12:44 PM
+class Solution:
+  def getSneakyNumbers(self, nums: list[int]) -> list[int]:
+    return [num for num, freq in collections.Counter(nums).items()
+            if freq == 2]
