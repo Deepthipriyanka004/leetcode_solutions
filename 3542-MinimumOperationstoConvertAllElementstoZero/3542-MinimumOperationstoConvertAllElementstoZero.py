2ccfc45b34f26391db38118db0b7fbb291379482
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-11-10 21:15:58 +0530

    Create 3542-MinimumOperationstoConvertAllElementstoZero

---
+++ 3542-MinimumOperationstoConvertAllElementstoZero/3542-MinimumOperationstoConvertAllElementstoZero.py
@@ -0,0 +1,14 @@
+# Last updated: 11/10/2025, 9:15:55 PM
+class Solution:
+  def minOperations(self, nums: list[int]) -> int:
+    ans = 0
+    stack = [0]
+
+    for num in nums:
+      while stack and stack[-1] > num:
+        stack.pop()
+      if not stack or stack[-1] < num:
+        ans += 1
+        stack.append(num)
+
+    return ans
