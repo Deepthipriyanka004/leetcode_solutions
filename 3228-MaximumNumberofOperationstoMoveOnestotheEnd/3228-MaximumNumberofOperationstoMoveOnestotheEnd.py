9ad10b8eea680364203854520f818d6c2d0a667b
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-11-13 15:09:03 +0530

    Create 3228-MaximumNumberofOperationstoMoveOnestotheEnd

---
+++ 3228-MaximumNumberofOperationstoMoveOnestotheEnd/3228-MaximumNumberofOperationstoMoveOnestotheEnd.py
@@ -0,0 +1,13 @@
+# Last updated: 11/13/2025, 3:08:56 PM
+class Solution:
+  def maxOperations(self, s: str) -> int:
+    ans = 0
+    ones = 0
+
+    for i, c in enumerate(s):
+      if c == '1':
+        ones += 1
+      elif i + 1 == len(s) or s[i + 1] == '1':
+        ans += ones
+
+    return ans
