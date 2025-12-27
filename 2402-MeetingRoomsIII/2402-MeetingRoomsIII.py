18c9ec96127798315199bd09d112bc45d11218d9
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2025-12-27 20:50:02 +0530

    File updated at 12/27/2025, 8:49:56 PM

--- 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -1,26 +1,26 @@
-# Last updated: 7/11/2025, 3:05:17 PM
-class Solution:
-  def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
-    count = [0] * n
-
-    meetings.sort()
-
-    occupied = []  # (endTime, roomId)
-    availableRoomIds = [i for i in range(n)]
-    heapq.heapify(availableRoomIds)
-
-    for start, end in meetings:
-      # Push meetings ending before this `meeting` in occupied to the
-      # `availableRoomsIds`.
-      while occupied and occupied[0][0] <= start:
-        heapq.heappush(availableRoomIds, heapq.heappop(occupied)[1])
-      if availableRoomIds:
-        roomId = heapq.heappop(availableRoomIds)
-        count[roomId] += 1
-        heapq.heappush(occupied, (end, roomId))
-      else:
-        newStart, roomId = heapq.heappop(occupied)
-        count[roomId] += 1
-        heapq.heappush(occupied, (newStart + (end - start), roomId))
-
-    return count.index(max(count))
+# Last updated: 12/27/2025, 8:49:56 PM
+1class Solution:
+2  def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
+3    count = [0] * n
+4
+5    meetings.sort()
+6
+7    occupied = []  # (endTime, roomId)
+8    availableRoomIds = [i for i in range(n)]
+9    heapq.heapify(availableRoomIds)
+10
+11    for start, end in meetings:
+12      # Push meetings ending before this `meeting` in occupied to the
+13      # `availableRoomsIds`.
+14      while occupied and occupied[0][0] <= start:
+15        heapq.heappush(availableRoomIds, heapq.heappop(occupied)[1])
+16      if availableRoomIds:
+17        roomId = heapq.heappop(availableRoomIds)
+18        count[roomId] += 1
+19        heapq.heappush(occupied, (end, roomId))
+20      else:
+21        newStart, roomId = heapq.heappop(occupied)
+22        count[roomId] += 1
+23        heapq.heappush(occupied, (newStart + (end - start), roomId))
+24
+25    return count.index(max(count))
