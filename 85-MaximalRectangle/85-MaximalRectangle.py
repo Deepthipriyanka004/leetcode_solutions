2d016962b3015ffc86cf34b05c402aba2dfefbab
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2026-01-11 10:40:32 +0530

    File updated at 1/11/2026, 10:40:14 AM

--- 85-MaximalRectangle/85-MaximalRectangle.py
+++ 85-MaximalRectangle/85-MaximalRectangle.py
@@ -1,30 +1,31 @@
-# Last updated: 6/2/2025, 6:40:39 PM
-class Solution:
-  def maximalRectangle(self, matrix: list[list[str]]) -> int:
-    if not matrix:
-      return 0
-
-    ans = 0
-    hist = [0] * len(matrix[0])
-
-    def largestRectangleArea(heights: list[int]) -> int:
-      ans = 0
-      stack = []
-
-      for i in range(len(heights) + 1):
-        while stack and (i == len(heights) or heights[stack[-1]] > heights[i]):
-          h = heights[stack.pop()]
-          w = i - stack[-1] - 1 if stack else i
-          ans = max(ans, h * w)
-        stack.append(i)
-
-      return ans
-
-    for row in matrix:
-      for i, num in enumerate(row):
-        hist[i] = 0 if num == '0' else hist[i] + 1
-      ans = max(ans, largestRectangleArea(hist))
-
-    return ans
-
-
+# Last updated: 1/11/2026, 10:40:14 AM
+1class Solution:
+2  def maximalRectangle(self, matrix: list[list[str]]) -> int:
+3    if not matrix:
+4      return 0
+5
+6    ans = 0
+7    hist = [0] * len(matrix[0])
+8
+9    def largestRectangleArea(heights: list[int]) -> int:
+10      ans = 0
+11      stack = []
+12
+13      for i in range(len(heights) + 1):
+14        while stack and (i == len(heights) or heights[stack[-1]] > heights[i]):
+15          h = heights[stack.pop()]
+16          w = i - stack[-1] - 1 if stack else i
+17          ans = max(ans, h * w)
+18        stack.append(i)
+19
+20      return ans
+21
+22    for row in matrix:
+23      for i, num in enumerate(row):
+24        hist[i] = 0 if num == '0' else hist[i] + 1
+25      ans = max(ans, largestRectangleArea(hist))
+26
+27    return ans
+28
+29
+30
