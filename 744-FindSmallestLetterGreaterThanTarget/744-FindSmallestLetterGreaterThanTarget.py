acf738c4a70ac2dbaeb10f16046b420058f4cd2f
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2026-01-31 21:19:20 +0530

    File updated at 1/31/2026, 9:19:17 PM

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
@@ -1,4 +1,4 @@
-# Last updated: 1/31/2026, 9:18:29 PM
+# Last updated: 1/31/2026, 9:19:17 PM
 1class Solution:
 2  def nextGreatestLetter(self, letters: list[str], target: str) -> str:
 3    l = bisect.bisect_right(range(len(letters)), target,
