f00aca7cea2dec30b6c022d012fa69b20ca71e43
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2026-04-04 22:14:08 +0530

    Create 2075-DecodetheSlantedCiphertext

---
+++ 2075-DecodetheSlantedCiphertext/2075-DecodetheSlantedCiphertext.py
@@ -0,0 +1,22 @@
+# Last updated: 4/4/2026, 10:13:07 PM
+1class Solution:
+2  def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+3    n = len(encodedText)
+4    cols = n // rows
+5
+6    ans = []
+7    matrix = [[' '] * cols for _ in range(rows)]
+8
+9    for i in range(rows):
+10      for j in range(cols):
+11        matrix[i][j] = encodedText[i * cols + j]
+12
+13    for col in range(cols):
+14      i = 0
+15      j = col
+16      while i < rows and j < cols:
+17        ans.append(matrix[i][j])
+18        i += 1
+19        j += 1
+20
+21    return ''.join(ans).rstrip()
