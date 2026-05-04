db396902249b81ae912fc5cb77056005880c83b5
Deepthi Priyanka <119839985+Deepthipriyanka004@users.noreply.github.com>
2026-05-04 23:38:49 +0530

    File updated at 5/4/2026, 11:38:47 PM

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,20 +1,17 @@
-// Last updated: 6/2/2025, 6:42:11 PM
-class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n >> 1; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int t = matrix[i][j];
-                matrix[i][j] = matrix[n - i - 1][j];
-                matrix[n - i - 1][j] = t;
-            }
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                int t = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = t;
-            }
-        }
-    }
-}
+// Last updated: 5/4/2026, 11:38:47 PM
+1class Solution {
+2  public void rotate(int[][] matrix) {
+3    for (int i = 0, j = matrix.length - 1; i < j; ++i, --j) {
+4      int[] temp = matrix[i];
+5      matrix[i] = matrix[j];
+6      matrix[j] = temp;
+7    }
+8
+9    for (int i = 0; i < matrix.length; ++i)
+10      for (int j = i + 1; j < matrix.length; ++j) {
+11        final int temp = matrix[i][j];
+12        matrix[i][j] = matrix[j][i];
+13        matrix[j][i] = temp;
+14      }
+15  }
+16}
